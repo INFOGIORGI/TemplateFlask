515fccaa134ff672092f80d169f67d2ff0293cf3
Luciano Schiavone <luciano.schiavone@ittgiorgi.edu.it>
2026-02-14 15:17:25 +0100

    Rename hello_world to home and enable debug mode

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home():
     return "<p>Hello, World!</p>"
 
-app.run()
+if __name__=='__main__':
+    app.run(debug=True)
